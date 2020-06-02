e700b24bfb3413ab4f68afc3857aa68b225a9185
Verena <meirelesverena@gmail.com>
2020-06-02 18:23:23 -0300

    added tests chapter 4

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
-class HomePageView(TemplateView):
-    template_name = 'homepage.html'
+#class HomePageView(TemplateView):
+#   template_name = 'homepage.html'
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'homepage.html'
